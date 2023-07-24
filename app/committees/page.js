ee8f02a9365cbd83464f300d71a41afe6ba52659
Kevin <kevin@beacons.ai>
2023-07-23 22:54:12 -0700

    some images

--- app/committees/page.js
+++ app/committees/page.js
@@ -1,5 +1,6 @@
 import { TypographyH1 } from '@/components/ui/typography/TypographyH1';
 import { TypographyH3 } from '@/components/ui/typography/TypographyH3';
+import Image from 'next/image';
 
 export const metadata = {
   title: 'Committees | THE BRIDGE',
@@ -12,36 +13,42 @@ const committees = [
     description:
       'A group of volunteers who are willing and able to set up, arrange, build, and tear down equipment, chairs, stages, tables, and other misc. tasks for different ministry events for church.',
     email: 'operations@nlvcbridge.com',
+    src: 'https://imagedelivery.net/_hFr1roF71hCDtIXUIgrjw/d63cbc6e-dd6b-4b73-4272-d27bbe620a00/public',
   },
   {
     title: 'MISSIONS',
     description:
       'A group of volunteers who help coordinate, plan, and support the local and overseas missions efforts of the Bridge. This includes supporting field missionaries with their needs as well as helping organize and launch short-term mission trips.',
     email: 'missions@nlvcbridge.com',
+    src: 'https://imagedelivery.net/_hFr1roF71hCDtIXUIgrjw/d506978d-2e1b-4c84-292a-982dc9659500/public',
   },
   {
     title: 'SPORTS',
     description:
       'A group of volunteers who help coordinate and plan our sports ministry of the Bridge. This includes supporting all sports and sports-related fundraisers.',
     email: 'sports@nlvcbridge.com',
+    src: 'https://imagedelivery.net/_hFr1roF71hCDtIXUIgrjw/d63cbc6e-dd6b-4b73-4272-d27bbe620a00/public',
   },
   {
     title: 'VISUAL ARTS',
     description:
       'A group of volunteers who are artistically gifted and willing to design, create, paint, make, build, and illustrate images for the church, both physically and digitally, that will enhance the aesthetics of the church ministry for the glory of God.',
     email: 'arts@nlvcbridge.com',
+    src: 'https://imagedelivery.net/_hFr1roF71hCDtIXUIgrjw/d63cbc6e-dd6b-4b73-4272-d27bbe620a00/public',
   },
   {
     title: 'WELCOME',
     description:
       'A group of volunteers who greet and welcome new guests to our Sunday worship service. This includes follow up and integration of new guests to the regular membership of the church.',
     email: 'welcome@nlvcbridge.com',
+    src: 'https://imagedelivery.net/_hFr1roF71hCDtIXUIgrjw/d63cbc6e-dd6b-4b73-4272-d27bbe620a00/public',
   },
   {
     title: "WOMEN'S MINISTRY",
     description:
       "A group of volunteers who help coordinate, plan, and support the Women's ministry of the Bridge. This includes soul care, Between Us events, and all other ministries focusing on the sisters of the Bridge.",
     email: 'women@nlvcbridge.com',
+    src: 'https://imagedelivery.net/_hFr1roF71hCDtIXUIgrjw/d63cbc6e-dd6b-4b73-4272-d27bbe620a00/public',
   },
 ];
 
@@ -53,10 +60,19 @@ export default function Committees() {
         {committees.map((committee) => {
           return (
             <div
-              className="h-60 md:h-80 cursor-pointer rounded-lg bg-black text-white flex items-center justify-center"
+              className="h-60 w-full md:h-80 cursor-pointer rounded-lg relative"
               key={committee.title}
             >
-              <TypographyH3>{committee.title}</TypographyH3>
+              <Image
+                alt={committee.title}
+                src={committee.src}
+                fill
+                objectFit="cover"
+                objectPosition="center"
+              />
+              <div className="z-10">
+                <TypographyH3>{committee.title}</TypographyH3>
+              </div>
             </div>
           );
         })}
--- app/live/page.js
+++ app/live/page.js
@@ -1,4 +1,8 @@
+import { Button } from '@/components/ui/button';
 import { TypographyH1 } from '@/components/ui/typography/TypographyH1';
+import { TypographyH2 } from '@/components/ui/typography/TypographyH2';
+import { Megaphone } from 'lucide-react';
+import Link from 'next/link';
 
 export const metadata = {
   title: 'Live | THE BRIDGE',
@@ -9,6 +13,28 @@ export default function Live() {
   return (
     <div className="container mx-auto py-12 sm:pb-24">
       <TypographyH1>Live Stream</TypographyH1>
+      <div className="mt-8">
+        <TypographyH2>Sundays at 11:30 AM</TypographyH2>
+      </div>
+      <div className="mt-8">
+        <iframe
+          width="100%"
+          height="720"
+          src="https://www.youtube.com/embed/dK_yrW8siyc"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        />
+      </div>
+      <div className="mt-8 flex justify-center">
+        <Link href="/announcements" passHref>
+          <Button>
+            <Megaphone className="mr-2 h-4 w-4" />
+            Announcements
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
