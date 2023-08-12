565e7a6fa76133429110ddc7f63a61c3579c9f19
Kevin <kevin@beacons.ai>
2023-08-12 09:18:31 -0700

    new map asset

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import HeroSection from '@/components/HeroSection';
 import { TypographyH1 } from '@/components/ui/typography/TypographyH1';
 import { TypographyP } from '@/components/ui/typography/TypographyP';
+import Image from 'next/image';
 
 export const metadata = {
   title: 'NLVC | THE BRIDGE',
@@ -12,16 +13,26 @@ export default function Home() {
   return (
     <div>
       <HeroSection />
-      <div className="container mx-auto py-36 md:py-40">
+      <div className="container mx-auto py-36 md:py-40 text-center">
         <div>
           <TypographyH1>Sunday Service</TypographyH1>
-          <TypographyP>Join us for worship at 11:30 AM</TypographyP>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52862.83851356888!2d-118.32276390929226!3d34.09700007358989!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2b8f560e36155%3A0xdba1381438aadcb2!2sNew%20Life%20Vision%20Church!5e0!3m2!1sen!2sus!4v1691258098945!5m2!1sen!2sus"
-            className="rounded-md mt-8 h-[450px] w-full md:w-1/2"
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+          <TypographyP>Join us for worship at 11:30 AM &#128522;</TypographyP>
+          <div className="flex flex-col md:flex-row justify-between items-center gap-4 mt-8">
+            <div className="relative h-60 sm:h-[400px] lg:h-[500px] w-full md:flex-1">
+              <Image
+                alt="directions to new life vision church"
+                src="/map.png"
+                fill
+                loading="lazy"
+              />
+            </div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d52862.83851356888!2d-118.32276390929226!3d34.09700007358989!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2b8f560e36155%3A0xdba1381438aadcb2!2sNew%20Life%20Vision%20Church!5e0!3m2!1sen!2sus!4v1691258098945!5m2!1sen!2sus"
+              className="h-60 sm:h-[400px] lg:h-[500px] w-full md:flex-1"
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            />
+          </div>
         </div>
         <div className="mt-20">
           <TypographyH1>Events</TypographyH1>
